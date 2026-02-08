eabbc3f1300ba0bc7eb7284740eb756272638f2b
Alireza Kashi <alireza.ark01@gmail.com>
2026-02-08 11:21:17 +0330

    add void logic
    add void logic from logic's branch

--- main_code.cpp
+++ main_code.cpp
@@ -225,6 +225,53 @@ void Input() {
 }
 
 
+void Logic(){
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+        }
+    
+    switch (dir) {
+    case LEFT:  x--; break;
+    case RIGHT: x++; break;
+    case UP:    y--; break;
+    case DOWN:  y++; break;
+    default:    break;
+    }
+
+    if (x >= width || x < 0 || y >= height || y < 0)
+        gameOver = true;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY) {
+        Beep(1600,35);
+        score += 10;
+
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+
+        if (nTail >= 1000)
+            nTail = 999;
+
+        gameDelay = max(50.0, gameDelay - 10.0);
+    }
+}
+
 int main() {
     bool appRunning = true;
 
